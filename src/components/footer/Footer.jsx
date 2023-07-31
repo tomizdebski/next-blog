63c96755b48aa06e4a10d34d89a02b6da734dd1a
tomizdebski <tomizdebski@gmail.com>
2023-07-31 13:06:13 +0200

    add_routing

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <div>2023 izdebski.dev. All rights rerserved.</div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Footer
